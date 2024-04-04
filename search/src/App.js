d56e57ff9989de71d79eb2fc73a8f5d6204bd3c1
Srijalsahu <srijalsahu007@gmail.com>
2024-04-04 13:00:28 +0530

    dddd

--- search/src/App.js
+++ search/src/App.js
@@ -1,11 +1,51 @@
-import "./App.css";
-import Countries from "./countries";
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch('https://restcountries.com/v3.1/all');
+      if (!response.ok) {
+        throw new Error('Failed to fetch countries');
+      }
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCountries = countries.filter(country =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <>
-    <Countries/>
-    </>
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search for a country..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <div className="countryContainer">
+        {filteredCountries.map(country => (
+          <div className="countryCard" key={country.cca3}>
+            <img src={country.flags.png} alt={country.name.common} />
+            <p>{country.name.common}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
